70c22e0618f693289b38c79c5ef45979ecb16c48
Ashish Mishra <ashishmishra9426@gmail.com>
2021-08-27 14:46:11 +0530

    petsTemplating

--- Day20 (25.08.2021) AZAX, JSON file reading/Task/Pet Registration/script.js
+++ Day20 (25.08.2021) AZAX, JSON file reading/Task/Pet Registration/script.js
@@ -4,6 +4,8 @@ event.preventDefault()
  
 var data=serialize(this)
 console.log(data)
+
+BindItem(data)
 })
 
 function serialize(frm){
@@ -16,3 +18,37 @@ function serialize(frm){
     return pets
 }
 
+
+function BindItem(arr){
+    var temp=``
+    arr.forEach((e)=>{
+
+        temp+=`<div class="col-4"><div class="card shadow">
+        <img src="" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h4 class="card-title">${e.name}</h4>
+            <p class="card-text">${e.address}</p>
+            <p class="card-text">${e.gender}</p>
+            <p class="card-text">${e.like}</p>
+            <p class="card-text">${e.weight}</p>
+            <p class="card-text">${e.type}</p>
+        </div>
+        <div class="card-body card-p">
+            <div class="row">
+                <div class="col col-xs-4 ">
+                    <i class="far fa-comments"></i> 456
+                </div>
+                <div class="col col-xs-4 ">
+                    <i class="far fa-heart"></i> 2.4k
+                </div>
+                <div class="col col-xs-4">
+                    <i class="fas fa-share"></i> 99
+                </div>
+            </div>
+        </div>
+    </div></div>`
+    })
+
+document.querySelector(".post").innerHTML=temp;
+}
+    
